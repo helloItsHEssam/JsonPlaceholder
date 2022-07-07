3e8ed6b09217bc7042a425456ec850c9f8be7c3c
Hessam Mahdiabadi <67460597+iamHEssam@users.noreply.github.com>
2022-07-07 17:05:52 +0430

    add repository

--- app/src/main/java/com/iamhessam/jsonplaceholder/data/local/LocalRepository.kt
+++ app/src/main/java/com/iamhessam/jsonplaceholder/data/local/LocalRepository.kt
@@ -1,9 +1,24 @@
 package com.iamhessam.jsonplaceholder.data.local
 
-import androidx.room.RoomDatabase
+import android.content.Context
+import androidx.room.Room
+import com.iamhessam.jsonplaceholder.data.local.db.room.AppDB
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class LocalRepository(room: RoomDatabase) {
+class LocalRepository @Inject constructor(@ApplicationContext private val context: Context) {
 
-    var appDB: RoomDatabase = room
+    lateinit var appDB: AppDB
         private set
+
+    init {
+        initRoomDB()
+    }
+
+    private fun initRoomDB() {
+        appDB = Room
+            .databaseBuilder(this.context, AppDB::class.java, "db")
+            .build()
+    }
+
 }
--- app/src/main/java/com/iamhessam/jsonplaceholder/data/local/db/room/AppDB.kt
+++ app/src/main/java/com/iamhessam/jsonplaceholder/data/local/db/room/AppDB.kt
@@ -1,8 +1,6 @@
 package com.iamhessam.jsonplaceholder.data.local.db.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.iamhessam.jsonplaceholder.data.local.db.room.dao.CommentDao
 import com.iamhessam.jsonplaceholder.data.local.db.room.entity.CommentEntity
@@ -11,20 +9,4 @@ import com.iamhessam.jsonplaceholder.data.local.db.room.entity.CommentEntity
 abstract class AppDB: RoomDatabase() {
 
     abstract fun commentDao(): CommentDao
-
-    companion object {
-        private const val dbName: String = "db"
-        private var instance_: AppDB? = null
-        fun getInstance(context: Context): AppDB {
-            if (instance_ == null) {
-                instance_ = createInstance(context)
-            }
-            return instance_!!
-        }
-
-        @Synchronized
-        private fun createInstance(context: Context): AppDB {
-            return Room.databaseBuilder(context, AppDB::class.java, dbName).build()
-        }
-    }
 }
